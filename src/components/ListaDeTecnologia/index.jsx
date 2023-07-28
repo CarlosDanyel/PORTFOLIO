b6897c76bd7f37a0af6a137fb025cf4c6fd9863d
CarlosDanyel <carlosdanyelsilva27@gmail.com>
2023-07-28 01:30:07 -0300

    bugSvg-v3

--- src/components/ListaDeTecnologia/index.jsx
+++ src/components/ListaDeTecnologia/index.jsx
@@ -1,6 +1,5 @@
 import Js from "../icon/Tecnologias/Js";
 import Css from "../icon/Tecnologias/css";
-import Git from "../icon/Tecnologias/git";
 import Html from "../icon/Tecnologias/html";
 import React from "../icon/Tecnologias/react";
 import Sass from "../icon/Tecnologias/sass";
@@ -10,7 +9,7 @@ const techData = [
   { name: "React", svg: <React/>,  duration: "100" } ,
   { name: "JavaScript", svg: <Js/>,  duration: "200" },
   { name: "Sass/Scss", svg: <Sass/>,  duration: "300" },  
-  { name: "Git", svg: <Git/>,  duration: "400" },
+  { name: "Git", svg: <Sass/>,  duration: "400" },
   { name: "HTML", svg: <Html/>,  duration: "500" },
   { name: "CSS", svg: <Css/>,  duration: "600" },
 ];
