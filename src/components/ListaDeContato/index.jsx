4069d81fc7d29a3c9197c812bb56e02327308954
CarlosDanyel <carlosdanyelsilva27@gmail.com>
2023-07-28 01:19:07 -0300

    bugGit

--- src/components/ListaDeContato/index.jsx
+++ src/components/ListaDeContato/index.jsx
@@ -1,7 +1,7 @@
 
 import Instagram from '../icon/Redes/instagram';
 import Linkedin from '../icon/Redes/linkedin';
-import Github from '../icon/Redes/Github';
+import Git from '../icon/Redes/git';
 
 import Email from '../icon/Redes/email';
 import Cell from '../icon/Redes/cell';
@@ -10,10 +10,11 @@ import style from "./style.module.scss";
 
 import { Link } from "react-router-dom";
 
+
 const contato = [
     {acesibilidade: "Visite o meu linkedin", name: "Linkedin", svg: <Linkedin/>, link: "https://www.linkedin.com/in/mano-deyvin-b78534278/"}, 
     {acesibilidade: "Visite o meu instagram", name: "Instagram", svg: <Instagram/>, link: "https://www.instagram.com/_danyelzs/" },
-    {acesibilidade: "Visite o meu github", name: "Github", svg: <Github/>, link: "https://github.com/CarlosDanyel" },
+    {acesibilidade: "Visite o meu github", name: "Github", svg: <Git/>, link: "https://github.com/CarlosDanyel" },
     {acesibilidade: "Meu numero +55 11 985673241", name: "+55 11 985673241", svg: <Cell/>, duration: "400" },
     {acesibilidade: "Meu email carlosdanyelsilva27@gmail.com", name: "carlosdanyelsilva27@gmail.com", svg: <Email/>},
   ];
