404f55c3ce0ea2a7acae2f1876a3b9aff2cb3dbb
CarlosDanyel <carlosdanyelsilva27@gmail.com>
2023-07-31 03:07:24 -0300

    adicionando slider

--- src/components/ListaDeTecnologia/index.jsx
+++ src/components/ListaDeTecnologia/index.jsx
@@ -4,29 +4,71 @@ import React from "../icon/Tecnologias/React";
 import Sass from "../icon/Tecnologias/Sass";
 import TechCss from "../icon/Tecnologias/TechCss";
 import TechGit from "../icon/Tecnologias/TechGit";
+import Java from "../icon/Tecnologias/Java";
+import NodeJS from "../icon/Tecnologias/NodeJS";
 
 import style from"./style.module.scss";
-  
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from "swiper";
+import MongoDb from "../icon/Tecnologias/MongoDb";
+
 const techData = [
-  { name: "React", svg: <React/>,  duration: "100" } ,
-  { name: "JavaScript", svg: <Js/>,  duration: "200" },
-  { name: "Sass/Scss", svg: <Sass/>,  duration: "300" },  
-  { name: "Git", svg: <TechGit/>,  duration: "400" },
-  { name: "HTML", svg: <Html/>,  duration: "500" },
-  { name: "CSS", svg: <TechCss/>,  duration: "600" },
+  { name: "JavaScript", svg: <Js/>},
+  { name: "React", svg: <React/> },
+  { name: "Node Js", svg: <NodeJS/> },
+  { name: "MongoDB", svg: <MongoDb/> },
+  { name: "Java", svg: <Java/>},
+  { name: "Sass/Scss", svg: <Sass/>},  
+  { name: "Git", svg: <TechGit/>},
+  { name: "HTML", svg: <Html/>},
+  { name: "CSS", svg: <TechCss/>},
 ];
 
 const ListaDeTecnologia = () => {
   
   const renderTechs = () => {
     return techData.map((tech, index) => (
-      <li key={index} data-name={tech.name} className={style.tech}>
+      <SwiperSlide key={index} data-name={tech.name} className={style.tech}>
         {tech.svg}
-      </li>
+      </SwiperSlide>
     ));
   };
 
-  return <ul className={style.containerStack}>{renderTechs()}</ul>;
+  return <div className={style.swiperContainer}>
+      <Swiper   
+    slidesPerView={4}
+    spaceBetween={25}
+    freeMode={true}
+    pagination={{
+      clickable: true,
+    }}
+    modules={[FreeMode]}
+    breakpoints={{
+      1200: {
+        slidesPerView: 4,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      540: {
+        slidesPerView: 3 ,
+      },
+      340: {
+        slidesPerView: 1,
+      },
+      140: {
+        slidesPerView: 1,
+      },
+    }}
+    className={style.containerStack}>{renderTechs()}
+    </Swiper>
+  </div>;
 };
 
 export default ListaDeTecnologia;
