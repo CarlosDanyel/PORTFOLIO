b8956c0251e01de2659774bc73736a6a5b294784
CarlosDanyel <carlosdanyelsilva27@gmail.com>
2023-11-18 23:59:21 -0300

    header remove

--- src/components/ListaLinksHeader/index.jsx
+++ src/components/ListaLinksHeader/index.jsx
@@ -1,16 +1,15 @@
 /* eslint-disable react/prop-types */
 import { useTranslation } from "react-i18next";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 // eslint-disable-next-line no-unused-vars
 import { Link, animateScroll as scroll } from "react-scroll";
 
 const ListaLinksHeader = (props) => {
   const { t } = useTranslation();
-  const location = useLocation();
-  const isHomePage = location.pathname === "/";
 
   const ListaDeLinks = [
     { name: t("link-header2"), to: t("link-header2") },
+    { name: t("link-header3"), to: t("link-header3") },
     { name: t("link-header4"), to: t("link-header4") },
     { name: t("link-header5"), to: t("link-header5") },
     { name: t("link-header7"), to: t("link-header7") },
@@ -24,27 +23,19 @@ const ListaLinksHeader = (props) => {
           {t("link-header1")}
         </NavLink>
       </li>
-      {isHomePage && (
-        <li>
-          <NavLink onClick={props.reactive} to={"/Blog"}>
-            {t("link-header3")}
-          </NavLink>
+      {ListaDeLinks.map((item, indice) => (
+        <li key={indice}>
+          <Link
+            onClick={props.reactive}
+            to={item.to}
+            spy={true}
+            smooth={true}
+            duration={1300}
+          >
+            {item.name}
+          </Link>
         </li>
-      )}
-      {isHomePage &&
-        ListaDeLinks.map((item, indice) => (
-          <li key={indice}>
-            <Link
-              onClick={props.reactive}
-              to={item.to}
-              spy={true}
-              smooth={true}
-              duration={1300}
-            >
-              {item.name}
-            </Link>
-          </li>
-        ))}
+      ))}
     </ul>
   );
 };
